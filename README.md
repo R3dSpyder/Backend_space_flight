961d65719d01e64faba955f36cfe449a22eb3484
R3dSpyder <carlmckeown1@gmail.com>
2022-09-09 17:17:04 +0100

    cleaned up environment variables, added skeleton README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+//to do- finish readme
--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,8 @@ const ENV = process.env.NODE_ENV || "development";
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-console.log(process.env.PGDATABASE);
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  // console.log(process.env.PGDATABASE)
   throw new Error(`PGDATABASE or DATABASE_URL not set, ${__dirname}`);
 }
 
