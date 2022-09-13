46c87248f9818c2379ea14884c2742f008980f8d
R3dSpyder <carlmckeown1@gmail.com>
2022-09-13 14:43:26 +0100

    fix

--- controllers/postScore.js
+++ controllers/postScore.js
@@ -2,11 +2,11 @@ const makeScore = require("../models/makeScore.js");
 
 const postScore = async (req, res, next) => {
   console.log(req.body);
-  if (req.body[0].score && req.body[0].username) {
+  if (req.body.score && req.body.username) {
     try {
-      const putScore = await makeScore(req.body[0].score, req.body.username);
+      const putScore = await makeScore(req.body.score, req.body.username);
       if (putScore.length > 0) {
-        res.status(201).send({ putScore: putScore[0] });
+        res.status(201).send({ putScore: putScore });
       }
     } catch (error) {
       res.status(400).send({ error: error });
