25803e7bffccaa56efe3e16b57dfc623bc2505db
R3dSpyder <carlmckeown1@gmail.com>
2022-09-13 13:54:21 +0100

    troubleshooting

--- models/makeScore.js
+++ models/makeScore.js
@@ -2,12 +2,13 @@ const db = require("../db/connection.js");
 
 const makeScore = async (score, user) => {
   try {
-    const nameQuery = `INSERT INTO users(username) VALUES('${user}') ON CONFLICT(username) DO NOTHING;`;
+    const nameQuery = `INSERT INTO users(username) VALUES('${user}') ON CONFLICT DO NOTHING;`;
     const checkIfNameExists = await db.query(nameQuery);
     const stringQuery = `INSERT INTO scores(score, user_id) VALUES (${score}, (SELECT user_id FROM users WHERE username='${user}')) RETURNING *;`;
     const insertScore = await db.query(stringQuery);
 
     if (insertScore.rows.length > 0) {
+      console.log(insertScore.rows);
       return insertScore.rows;
     }
   } catch (error) {
